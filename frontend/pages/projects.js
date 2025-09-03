83c7eb8845a5d9afc37e190f4537be9f7afea345
ARazak89 <hassane.abdelrazak@gmail.com>
2025-09-03 16:37:56 +0100

    fix: Pass correct projectId to availability API in projects.js

--- frontend/pages/projects.js
+++ frontend/pages/projects.js
@@ -394,7 +394,7 @@ function ProjectsPage() {
       const token = getAuthToken();
       // Charger les slots disponibles pour ce projet
       // La route doit maintenant prendre projectId et assignmentId
-      const slotsRes = await fetch(`${API}/api/availability/available-for-project/${project.projectId}/${project.assignmentId}`, {
+      const slotsRes = await fetch(`${API}/api/availability/available-for-project/${project._id}/${project.assignmentId}`, {
         headers: { Authorization: `Bearer ${token}` }
       });
       if (slotsRes.ok) {
